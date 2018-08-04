2351be02ce3f3639738295b71471f8391a3457b6
lishnalei <2686685661@qq.com>
2018-08-04 15:33:53 +0800

    first pull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="X-CSRF-TOKEN" content="{{csrf_token()}}">
-
+        <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+        <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+        <!-- <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
         <title>Laravel</title>
 
     </head>
-    <body>
+    <body style="margin:0">
         <div id="app"></div>
         
         <script src="{{ asset('js/app.js') }}"></script>
